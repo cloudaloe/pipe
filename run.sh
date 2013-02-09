d1560692feef3cf6d0466317929da84743740ae7
matan <matan@cloudaloe.org>
2013-02-09 18:07:56 +0200

    nc

---
+++ run.sh
@@ -0,0 +1,1 @@
+sbt -mem 256 run
---
+++ set-scala-JVM.sh
@@ -0,0 +1,4 @@
+echo "run this script with source or . preceded to it, in order for it to export into the shell."
+echo "that will make scala use the JVM exported to the environment here, rather than the OS default one (the OS default is of course what you get when running 'java')"
+export JAVA_HOME=/usr/local/oracleJDK/jdk1.7.0_13
+
