d6513f24524050a7a9095a379f3cae0da1b4cbd0
matan <matan@cloudaloe.org>
2013-02-12 16:27:38 +0200

    nc

--- src/main/scala/pipe/CloudReceiver.scala
+++ src/main/scala/pipe/CloudReceiver.scala
@@ -4,8 +4,12 @@
 
 package pipe
 
+
 import org.jboss.netty.bootstrap.ServerBootstrap
 import org.jboss.netty.channel.{ ChannelPipeline, ChannelPipelineFactory, Channels }
+import org.jboss.netty.channel.ChannelHandlerContext
+import org.jboss.netty.channel.ExceptionEvent
+import org.jboss.netty.channel.MessageEvent
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory
 import org.jboss.netty.channel.SimpleChannelUpstreamHandler
 import org.jboss.netty.handler.codec.http._
@@ -26,15 +30,38 @@ import org.jboss.netty.handler.ssl.SslHandler
 class HttpServerPipelineFactory extends ChannelPipelineFactory {
   
 	/**
-	* Create an SSLEngine for netty to use. 
+	* Creates an SSLEngine for netty to use.
+	*  
 	* Netty needs this set up for it before letting it run SSL.
 	* At run-time, The SSL handshake may fail due to any of a myriad of issues on either network side.
 	* On failure, the Java infrastructure will not always surface enough details of the root cause (such as the handshake process details),  
 	* And so elevated logging through -Djavax.net.debug=ssl:handshake may be used for analysis.
 	* 
+	* 'SSL family' Algorithms that can be chosen are found (respectively of Java version) at:
+	* http://docs.oracle.com/javase/6/docs/technotes/guides/security/StandardNames.html#Cipher and 
+	* http://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#SSLContext 
+	* 
 	* TODO: figure a way of logging handshake details only for failed handshakes (stackoverflow). 
+	* 		or enable orderly logging of all handshakes into a designated log
 	*/
-		
+
+	val sslContext : SSLContext = SSLContext.getInstance("TLS")
+	//ssl sslContext : SSLContext = SSLContext.getInstance("SSLv3") 
+	  
+	// Load the keystore from disk
+	val keyStore = KeyStore.getInstance("JKS")
+	val keyStoreFile = new FileInputStream("keystore.jks") 
+	val keyStorePassword = "password"
+	keyStore.load(keyStoreFile, keyStorePassword.toCharArray())
+	keyStoreFile.close()
+
+	// initialize both a KeyManagerFactory and a TrustManagerFactory with the key store
+	val keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm)
+	val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
+	keyManagerFactory.init(keyStore, keyStorePassword.toCharArray())
+	trustManagerFactory.init(keyStore)   
+	sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null) // need to enter keystore, cipher suites etc through these params
+	
 	def getSslEngine: SSLEngine = {
 	  
    	  //val sslContext : SSLContext = SSLContext.getDefault
@@ -43,27 +70,7 @@ class HttpServerPipelineFactory extends ChannelPipelineFactory {
 	  //println("supported: " + sslContext.getSupportedSSLParameters.toString())
 	  //println("supported cipher suites: " + sslEngine.getSupportedCipherSuites.mkString(", "))
 	  //println("enabled cipher suites: " + sslEngine.getEnabledCipherSuites.mkString(", "))
-	  	  
-	  // Algorithms that can be chosen are found at:
-	  // http://docs.oracle.com/javase/6/docs/technotes/guides/security/StandardNames.html#Cipher and 
-	  // http://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#SSLContext respectively
-	  val sslContext : SSLContext = SSLContext.getInstance("TLS")
-	  //ssl sslContext : SSLContext = SSLContext.getInstance("SSLv3") 
-	  
-	  // Load the keystore from disk
-	  val keyStore = KeyStore.getInstance("JKS")
-	  val keyStoreFile = new FileInputStream("keystore.jks") 
-	  val keyStorePassword = "password"
-	  keyStore.load(keyStoreFile, keyStorePassword.toCharArray())
-      keyStoreFile.close()
-      
-      // initialize both a KeyManagerFactory and a TrustManagerFactory with the key store
-      val keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm)
-      val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
-      keyManagerFactory.init(keyStore, keyStorePassword.toCharArray())
-      trustManagerFactory.init(keyStore)   
-	  sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null) // need to enter keystore, cipher suites etc through these params
-	 	  
+     	  
 	  val sslEngine : SSLEngine = sslContext.createSSLEngine()
 	  sslEngine.setUseClientMode(false) 	// javax.net.ssl.SSLEngine's special way of saying 'play a server on the SSL handshake'
 	  sslEngine.setNeedClientAuth(false) 	// turn to 'true' to require client auth (http://docs.oracle.com/javase/6/docs/api/javax/net/ssl/SSLEngine.html#setNeedClientAuth(boolean))
@@ -80,7 +87,7 @@ class HttpServerPipelineFactory extends ChannelPipelineFactory {
 	  // as without it println(sslHandler.getCloseOnSSLException) still shows that this defaults to false.
 	  sslHandler.setCloseOnSSLException(true);   
 	  
-	  pipeline.addLast("ssl", sslHandler);
+	  //pipeline.addLast("ssl", sslHandler);
       pipeline.addLast("decoder", new HttpRequestDecoder())
       // Uncomment the following line if you don't want to handle HttpChunks.
       //pipeline.addLast("aggregator", new HttpChunkAggregator(1048576));
@@ -93,7 +100,11 @@ class HttpServerPipelineFactory extends ChannelPipelineFactory {
 }
 
 class HttpRequestHandler extends SimpleChannelUpstreamHandler {
-	println("http request received")
+	println("Http connection made")
+	override def messageReceived(channelHandlerContext: ChannelHandlerContext, messageEvent: MessageEvent){
+	  println("Http connection made from " + messageEvent.getRemoteAddress.toString)
+	  println("Http message received: \n" + messageEvent.getMessage.toString)
+	} 
 }
 
 class CloudReceiver {
