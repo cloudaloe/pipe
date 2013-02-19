2cc784779b962cb6c83d65a14ba60f8f505dc5d9
matan <matan@cloudaloe.org>
2013-02-19 08:47:32 +0200

    nc

--- src/main/scala/Broker.scala
+++ src/main/scala/Broker.scala
@@ -22,15 +22,18 @@ import io.netty.channel.ChannelInboundMessageHandlerAdapter
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelFutureListener
 
+
+
 /**
- * Setup a client pipeline 
+ * Send out to the cloud
  */
 
 class Broker (incomingPort: Int, cloudPort: Int, ssl: Boolean) {
+  
+	/**
+	 * Setup a client pipeline 
+	 */
 	class clientInitializer(ssl: Boolean) extends ChannelInitializer[Channel] {
-	
-	  //override def inboundBufferUpdate(ctx: ChannelHandlerContext, in: ByteBuf){}
-	  
 	  override def initChannel(channel: Channel){
 	    val pipeline = channel.pipeline
 	    if (ssl){
@@ -57,50 +60,63 @@ class Broker (incomingPort: Int, cloudPort: Int, ssl: Boolean) {
 	}  
 	
 	/** 
-	 * handle server's http response
+	 * handle server's http response or connection errors
 	 */
 	class httpClientHandler extends ChannelInboundMessageHandlerAdapter[HttpResponse]{
-
-	override def messageReceived(ctx: ChannelHandlerContext, response: HttpResponse){
-		println(response.getStatus)
-	}
-  
-	override def exceptionCaught(ctx: ChannelHandlerContext, e: Throwable){
-		println(e.printStackTrace)
-	}	
+	  
+		override def messageReceived(ctx: ChannelHandlerContext, response: HttpResponse){
+			println(response.getStatus)
+		}
 		
+		override def exceptionCaught(ctx: ChannelHandlerContext, e: Throwable){
+			println(e.printStackTrace)
+		}	
 	}
 	
-	// netty will pick the local port for outgoing communication to the cloud side - automatically
-	// this behavior can of course be changed.
-    
+	object writer{
+	  var canWrite=false
+	  def write(msg: String){
+		  if (canWrite){
+	 		  var request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/hello")
+	 		  //request.headers.set(HttpHeaders.Names.HOST, "localhost")
+	 		  //request.headers.set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE)
+	 		  //request.headers.set(HttpHeaders.Names.ACCEPT_ENCODING, HttpHeaders.Values.GZIP)
+	 		  var writeFuture = channel.write(request)
+	 		  //channel.flush
+	 		  //channel.closeFuture.sync
+	 		  //channelFuture.await(7, TimeUnit.SECONDS)
+	 		  //println("isDone :", channelFuture.isDone)
+	 		  //println("isSuccess :", channelFuture.isSuccess)
+		  }
+		  else println("channel is not ready for writing yet")
+	  }
+	}
+	
  	println("Broker object starting")
-				
+
+ 	/**
+ 	 * Initialize a single outgoing connection to the cloud
+ 	 */
 	val outgoingListener = new Bootstrap()
  		outgoingListener.group(new NioEventLoopGroup)
  						.channel(classOf[NioSocketChannel])
  						.handler(new clientInitializer(ssl))
+ 						
+ 	var channel: Channel = _
 
+ 	// Netty will pick the local port for outgoing communication to the cloud side - automatically. This behavior can of course be changed. 	
  	val connectFuture = outgoingListener.connect(new InetSocketAddress("localhost", cloudPort)).addListener(new ChannelFutureListener(){
 		    def operationComplete(channelFuture: ChannelFuture){
-			  	println(channelFuture.channel.isOpen.toString + channelFuture.channel.isActive.toString + channelFuture.channel.isRegistered.toString + channelFuture.channel.localAddress + channelFuture.channel.remoteAddress)
+			  	//println(channelFuture.channel.isOpen.toString + channelFuture.channel.isActive.toString + channelFuture.channel.isRegistered.toString + channelFuture.channel.localAddress + channelFuture.channel.remoteAddress)
 			 	if (!channelFuture.channel.isOpen()) {
-			      println("Connection to cloud receiver failed: ") // + future.getCause + future.getCause.printStackTrace)
+			      println("Connection to cloud receiver failed.") // + future.getCause + future.getCause.printStackTrace)
 			    } //TODO: also check for success v.s. not completed. Will this hang if the server stalls?
-			 	
-				var request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/hello")
-				//request.headers.set(HttpHeaders.Names.HOST, "localhost")
-			    //request.headers.set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE)
-			    //request.headers.set(HttpHeaders.Names.ACCEPT_ENCODING, HttpHeaders.Values.GZIP)
-				//println(request.getMethod)
-				//println(request.getProtocolVersion)
-				//println(request.getUri)
-				var writeFuture = channelFuture.channel.write(request)
-				//channel.flush
-				//channel.closeFuture.sync
-				channelFuture.await(7, TimeUnit.SECONDS)
-				println("isDone :", channelFuture.isDone)
-				println("isSuccess :", channelFuture.isSuccess)
+			 	else {
+				 	channel = channelFuture.channel
+				 	writer.canWrite=true
+			 	}
 		    }
-		  })				
+		  })
+
+	
 }
